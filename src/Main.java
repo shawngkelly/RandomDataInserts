fba843b6e2f9cddbe9791d554fcd453d165c1856
shawngkelly <shawngkelly@hotmail.com>
2017-02-23 19:40:06 -0500

    Able to select different habits for different types of updates.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 /**
  * Created by Shawn on 2/23/2017.
  */
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Random;
 import java.util.Scanner;
 import java.sql.Date;
@@ -10,14 +14,13 @@ import java.time.*;
 public class Main {
 
     //Create scanner object to select parameters for input;
-    Scanner keyboard = new Scanner(System.in);
 
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         Scanner keyboard = new Scanner(System.in);
         int userID;
         int habitID;
-        Date date;
         int numRecords;
         int days;
         double packsDay;
@@ -28,28 +31,53 @@ public class Main {
         userID = keyboard.nextInt();
         System.out.println("Enter habitID");
         habitID = keyboard.nextInt();
-        System.out.println("Enter number of packs a  day ?");
-        packsDay = keyboard.nextDouble();
-        System.out.println("Enter how many days to create records for?");
-        days = keyboard.nextInt();
-
-        numRecords = (int)((packsDay*20) * days);
+        if (habitID == 1) {
+            System.out.println("Enter number of packs a  day ?");
+            packsDay = keyboard.nextDouble();
+            System.out.println("Enter how many days to create records for?");
+            days = keyboard.nextInt();
+            numRecords = (int)((packsDay*20) * days);
+        }
+        else if (habitID == 2) {
+            System.out.println("Enter number of dip cans a day");
+            double cans = keyboard.nextDouble();
+            System.out.println("How many days of records?");
+            days = keyboard.nextInt();
+            numRecords = (int)((cans*5)* days);
+        }
+        else if (habitID == 3) {
+            System.out.println("How many drinks per sitting?");
+            int drinks = keyboard.nextInt();
+            System.out.println("How many nights do you drink");
+            days = keyboard.nextInt();
+            numRecords = (drinks * days);
+        }
+        else if (habitID == 4) {
+            System.out.println("How many drinks per day?");
+            int drinks = keyboard.nextInt();
+            System.out.println("how many days do you drink?");
+            days = keyboard.nextInt();
+            numRecords = (drinks * days);
+        }
+        else {
+            return;
+        }
 
         System.out.println("Generating " + numRecords + " records of user " + userID + " with habit of " + habitID + ".");
+        FileWriter outPutFile = new FileWriter("trial.txt", true);
 
-
-
-        if (habitID == 1) {
-            for( int i = 0; i < numRecords; i++){
-                int hours = rnd.nextInt(17)+ 8;
+        for( int i = 0; i < numRecords; i++){
+                int hours = rnd.nextInt(18)+ 12;
                 int daysBack = rnd.nextInt((days)+1);
                 int minsBack = rnd.nextInt((60)+1);
                 int secondsBack = rnd.nextInt((60)+1);
-                System.out.println("INSERT INTO track_habits");
-                System.out.println("VALUES (" + userID + ", " + habitID + ", '" +
-                LocalDateTime.now().minusDays(daysBack).minusHours(hours).minusMinutes(minsBack).minusSeconds(secondsBack) + "');" );
+                outPutFile.append("INSERT INTO track_habits\n");
+                outPutFile.append("VALUES (" + userID + ", " + habitID + ", '" +
+                LocalDateTime.now().minusDays(daysBack).plusHours(hours).minusMinutes(minsBack).minusSeconds(secondsBack) + "');\n" );
             }
+        outPutFile.close();
+
         }
     }
 
-}
+
